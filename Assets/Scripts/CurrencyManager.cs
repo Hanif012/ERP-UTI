39375aa93200d0e7d6d75b1552b583b2ea1f8a1b
Austrooo <ericdjakaria04@gmail.com>
2024-04-14 15:56:04 +0700

    feat: Currency Formating
    Formating 1000 to Thousands and etc.
    Adding Scripts to Objects for further data

--- Assets/Scripts/CurrencyManager.cs
+++ Assets/Scripts/CurrencyManager.cs
@@ -8,13 +8,21 @@ public class CurrencyManager : MonoBehaviour
     public double currentMoney = 0;
     public TextMeshProUGUI CurrencyText;
 
-    void displayMoney()
-    {
-        CurrencyText.text = "$" + currentMoney.ToString("F2");
-    }
-
     private void Update()
     {
-        displayMoney();
+        CurrencyText.text = "Rp " + CurrencyFormat();
+    }
+
+    public string CurrencyFormat()
+    {   
+        string[] suffix = { "", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion", "Undecillion", "Duodecillion", "Tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septendecillion", "Octodecillion", "Novemdecillion", "Vigintillion", "Unvigintillion", "Duovigintillion", "Tresvigintillion", "Quattuorvigintillion", "Quinquavigintillion", "Sesvigintillion", "Septemvigintillion", "Octovigintillion", "Novemvigintillion", "Trigintillion", "Duotrigintillion" };
+        int i=0;
+        double amount = currentMoney;
+        for(i=0; amount >= 1000; i++)
+        {
+            amount /= 1000;
+        }
+
+        return amount.ToString("F2") + " " + suffix[i];
     }
 }
