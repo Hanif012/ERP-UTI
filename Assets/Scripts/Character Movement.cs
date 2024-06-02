9ef0f45676f1a18a232ad8e4b4b7a9f13f7f855c
Austrooo <ericdjakaria04@gmail.com>
2024-06-02 10:50:30 +0700

    Trying to Add Animation
    Animation need fixing
    Spawning is resolved

--- Assets/Scripts/Character Movement.cs
+++ Assets/Scripts/Character Movement.cs
@@ -10,30 +10,58 @@ public class CharacterMovement : MonoBehaviour
 {
     public CharacterSpawner characterSpawner;
     public GameObject point;
-    public float character_speed = 0.01f;
+    public float character_speed = 1f;
     private int state = 0;
+    private bool isWaiting = false;
+
     private void Update() {
-        float arrivalThreshold = 0.01f;
+        if(characterSpawner.spawnedCharacter == null) {
+            return;
+        }
+        ManageState();
+        MoveCharacter();
+    }
+
+    private IEnumerator WaitAndIncrementState(){
+        isWaiting = true;
+        yield return new WaitForSeconds(3);
+        state++;
+        isWaiting = false;
+    }
+
+    private void ManageState() {
+        float arrivalThreshold = 0.1f;
+
+        Debug.Log(isWaiting);
+
+        if(state >= point.transform.childCount) {
+            state = 0;
+        }
         if(state == 0){
-            state = 1;
+            if(Vector3.Distance(characterSpawner.spawnedCharacter.transform.position, point.transform.GetChild(state).position) < arrivalThreshold){
+                state = 1;
+            }
         }
         if(state == 1){
             if(Vector3.Distance(characterSpawner.spawnedCharacter.transform.position, point.transform.GetChild(state).position) < arrivalThreshold){
-                state = 2;
+                if (!isWaiting) {
+                    StartCoroutine(WaitAndIncrementState());
+                }
             }
         }
         if(state == 2){
             if(Vector3.Distance(characterSpawner.spawnedCharacter.transform.position, point.transform.GetChild(state).position) < arrivalThreshold){
                 Destroy(characterSpawner.spawnedCharacter);
                 characterSpawner.isObjectSpawned = false;
+                characterSpawner.vehicleIndex++;
             }
         }
-        MoveCharacter(state);
     }
 
-    void MoveCharacter(int pos) {
+    void MoveCharacter() {
         float speed = character_speed * Time.deltaTime / 3;
-        characterSpawner.spawnedCharacter.transform.position = Vector3.MoveTowards(characterSpawner.spawnedCharacter.transform.position, point.transform.GetChild(pos).position, speed);
+        characterSpawner.spawnedCharacter.transform.position = Vector3.MoveTowards(characterSpawner.spawnedCharacter.transform.position, point.transform.GetChild(state).position, speed);
     }
+
 }
     
--- Assets/Scripts/CharacterSpawner.cs
+++ Assets/Scripts/CharacterSpawner.cs
@@ -1,29 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class CharacterSpawner : MonoBehaviour
 {
     public GameObject Spawnpoint;
-    public GameObject characterPrefab;
+    public GameObject[] vehicleData;
+    public GameObject[] vehiclePrefab;
+    private bool[] isBought;
+    public int vehicleIndex = 0;
     public GameObject spawnedCharacter;
     public bool isObjectSpawned = false;
+
     void Start()
     {
-        SpawnVehicle();
-        isObjectSpawned = true;
+        isBought = new bool[vehicleData.Length];
+        for (int i = 0; i < vehicleData.Length; i++)
+        {
+            isBought[i] = vehicleData[i].GetComponent<Vehicle>().IsBought;
+        }
     }
     void Update()
     {
-        if(isObjectSpawned == false)
+
+        for (int i = 0; i < vehicleData.Length; i++)
         {
-            SpawnVehicle();
-            isObjectSpawned = true;
+            isBought[i] = vehicleData[i].GetComponent<Vehicle>().IsBought;
+        }
+
+        if(vehicleIndex >= vehicleData.Length)
+        {
+            vehicleIndex = 0;
+        }
+
+        if(!isObjectSpawned){
+            if(isBought[vehicleIndex]){
+                SpawnVehicle(vehiclePrefab[vehicleIndex]);
+                isObjectSpawned = true;
+            }
+            else{
+                vehicleIndex++;
+            }
         }
     }
 
-    public void SpawnVehicle()
-    {
-        spawnedCharacter = Instantiate(characterPrefab, Spawnpoint.transform.position, Quaternion.identity);
+    public void SpawnVehicle(GameObject vehicle)
+    { 
+        spawnedCharacter = Instantiate(vehicle, Spawnpoint.transform.position, Quaternion.identity);
     }
 }
