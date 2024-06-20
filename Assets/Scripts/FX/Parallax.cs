fac0da34a6fc9b6f8859aa455dbc63a92ccb5ecf
Hanif <andque12345012@gmail.com>
2024-06-20 19:31:42 +0700

    Add Timeline Chart

--- Assets/Scripts/FX/Parallax.cs
+++ Assets/Scripts/FX/Parallax.cs
@@ -1,36 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public class WaveMovement : MonoBehaviour
 {
-   private float _startingPost;
-   private float _lengthSprite;
-   public float ParallaxAmount;
-   public Camera MainCam;
+    public float speed = 2.0f; // Speed of the wave movement
+    public float waveWidth = 10.0f; // Width of the wave (how far it moves before looping)
+    private Vector3 startPosition;
 
-   private void Start()
-   {
-        _startingPost = transform.position.x;
+    private float _lengthSprite;
+    
+    void Start()
+    {
+        startPosition = transform.position;
         _lengthSprite = GetComponent<SpriteRenderer>().bounds.size.x;
-   }
-   private void Update()
-   {
-        Vector3 position = MainCam.transform.position;
-        float temp = position.x*(1-ParallaxAmount);
-        float distance = position.x * ParallaxAmount;
+    }
 
-        Vector3 NewPos = new Vector3(_startingPost + distance, transform.position.y, transform.position.z);
-
-        transform.position = NewPos;
-
-        if(temp > _startingPost + _lengthSprite / 2)
-        {
-            _startingPost += _lengthSprite;
-        }
-        else if(temp < _startingPost - _lengthSprite / 2)
-        {
-            _startingPost -= _lengthSprite;
-        }
-   }
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * speed, _lengthSprite);
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
 }
